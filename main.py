74c7b5f4daad4dd445eb7d9bbc443f485c145f05
Julie Saubot <juliesaubot@hotmail.com>
2024-02-12 23:42:42 +0100

    modify file

--- main.py
+++ main.py
@@ -31,9 +31,12 @@ print('Dataframe de données à prédire :')
 print('X_test shape : ', X_test.shape)
 
 #on charge le modèle final
-with open("model.pkl", 'rb') as pickle_in:
-    print("utilisation du model lightgbm")
-    grid_lgbm = pickle.load(pickle_in)
+#with open("model.pkl", 'rb') as pickle_in:
+#    print("utilisation du model lightgbm")
+#    grid_lgbm = pickle.load(pickle_in)
+
+grid_lgbm = pickle.load(open("model.pkl", 'rb'))
+
     
 #Application du modèle lgbm avec seuil calculé précédemment et détermination de la probabilité
 y_predict_prob = grid_lgbm.predict_proba(X_test)[:,1]
